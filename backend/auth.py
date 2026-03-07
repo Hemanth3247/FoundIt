32f37202c97dcdd2ff86a74f483dcd74257c9b96
Hemanth <hemanthvasamsetti3247@gmail.com>
2026-03-08 02:03:43 +0530

    Authentication Done

---
+++ backend/auth.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append("../")
+
+from database import usersdb
+
+def authenticate(doc, passhash):
+
+    response = usersdb.fetch_user(doc)
+    password_hash = response['password_hash']
+
+    if passhash == password_hash:
+        return True
+    else:
+        return False
---
+++ backend/server.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+from database import usersdb
+from backend import auth
+from datetime import datetime, timedelta, UTC
+from jose import jwt
+import sys
+
+sys.path.append("../")
+
+app = FastAPI()
+
+class SignUp(BaseModel):
+    username: str
+    email: str
+    phone: int
+    password_hash: str
+
+class Login(BaseModel):
+    
+    email: str | None = None
+    phone: int | None = None
+    password_hash: str
+
+SECRET = "FoundItSecretKey"
+
+def generate_JWT(username):
+    payload = {
+        'sub': username,
+        'iat':  datetime.now(UTC),
+        'exp': datetime.now(UTC) + timedelta(hours=1)
+    }
+
+    jwt_token = jwt.encode(payload,SECRET,"HS256")
+    return jwt_token
+
+@app.post('/signup')
+def signup(body: SignUp,response: Response):
+    
+    usersdb.add_user(
+        username = body.username,
+        email = body.email, 
+        phone = body.phone, 
+        password_hash = body.password_hash)
+        
+    token = generate_JWT(body.username)
+    response.set_cookie(key="token", token=token)
+
+@app.post('/login')
+def login(body: Login,response: Response):
+    
+    edoc = {'email': body.email}
+    pdoc = {'phone': body.phone}
+    if body.email:
+        doc = edoc
+    elif body.phone:
+        doc = pdoc
+    
+    user = auth.authenticate(doc,body.password_hash)
+
+    if user:
+        data = usersdb.fetch_user(doc)
+    else:
+        return "Incorrect Credentials"
+    
+    token = generate_JWT(data['username'])
+    response.set_cookie(key="token", token=token)
